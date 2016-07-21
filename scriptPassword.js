8232afced07cc9f7ee13579431537d9759234ca6
Codati <codatitiss@gmail.com>
2016-07-21 20:39:25 +0200

    hash password

---
+++ scriptPassword.js
@@ -0,0 +1,48 @@
+
+var crypto = require('crypto');
+
+
+module.exports.hash = function (pass) {
+  var h = crypto.createHash('sha512');
+  var salt = crypto.randomBytes(256);
+
+  h.update(pass);
+
+  h.update(salt);
+
+  var hash = h.digest();
+
+  return Buffer.concat([hash, salt]);
+
+};
+
+
+module.exports.testhash = function (pass, crypt) {
+  
+  if (crypt.length !== 320) {
+    return false;
+  }
+
+  var hash = crypt.slice(0, 64);
+  var salt = crypt.slice(64);
+
+
+  var h = crypto.createHash('sha512');
+
+  h.update(pass);
+
+  h.update(salt);
+
+  return Buffer.compare(hash, h.digest()) === 0;
+};
+
+module.exports.randomGenerate = function () {
+
+  var randomBuf = crypto.randomBytes(16)
+  var timetempsBuf = new Buffer(8);
+  timetempsBuf.writeUIntLE(Date.now(), 0, 8);
+  console.log(timetempsBuf.toString('base64'));
+  var result = Buffer.concat([randomBuf, timetempsBuf]).toString('base64');
+
+  return result;
+};
